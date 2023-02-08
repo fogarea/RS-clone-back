22fa5fb6732799c3c31d157a9ca4c6946ec1f8bd
Elijah <elijah.ivanik@inbox.ru>
2023-02-08 09:47:12 +0300

    refactor: change Progress scheme, add finished array

--- model/schema/progress.js
+++ model/schema/progress.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const Progress = new mongoose.Schema({
   workouts: { type: Number, required: true, default: 0 },
   watched: { type: Number, required: true, default: 0 },
-  calories: { type: Number, required: true, default: 0 }
+  calories: { type: Number, required: true, default: 0 },
+  finished: [{ type: String, required: false, ref: "Training" }]
 });
 
 export default mongoose.model("Progress", Progress);
