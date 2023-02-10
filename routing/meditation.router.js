1d674e9cf217f8fdc946ceb1e934c22b8ff06136
Elijah <elijah.ivanik@inbox.ru>
2023-02-10 14:44:50 +0300

    refactor: add withAuth middleware to Meditations

--- routing/meditation.router.js
+++ routing/meditation.router.js
@@ -1,11 +1,28 @@
 import { Router } from "express";
+import withAuth from "../middleware/auth.middleware.js";
 import meditationController from "../controller/meditation.controller.js";
 
 const router = new Router();
 
-router.get("/meditations/:id", meditationController.get.bind(meditationController));
-router.post("/meditations", meditationController.create.bind(meditationController));
-router.put("/meditations", meditationController.update.bind(meditationController));
-router.delete("/meditations/:id", meditationController.delete.bind(meditationController));
+router.get(
+  "/meditations/:id",
+  withAuth(true),
+  meditationController.get.bind(meditationController)
+);
+router.post(
+  "/meditations",
+  withAuth(true),
+  meditationController.create.bind(meditationController)
+);
+router.put(
+  "/meditations",
+  withAuth(true),
+  meditationController.update.bind(meditationController)
+);
+router.delete(
+  "/meditations/:id",
+  withAuth(true),
+  meditationController.delete.bind(meditationController)
+);
 
 export default router;
