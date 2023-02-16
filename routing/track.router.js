cf89e5604757c86b66434c71a9139fadae9e7e3c
Elijah <elijah.ivanik@inbox.ru>
2023-02-16 11:59:06 +0300

    refactor: comment Track edit/create routes

--- routing/track.router.js
+++ routing/track.router.js
@@ -4,7 +4,7 @@ import trackController from "../controller/track.controller.js";
 const router = new Router();
 
 router.get("/tracks", trackController.get.bind(trackController));
-router.put("/tracks", trackController.update.bind(trackController));
-router.post("/tracks", trackController.create.bind(trackController));
+//router.put("/tracks", trackController.update.bind(trackController));
+//router.post("/tracks", trackController.create.bind(trackController));
 
 export default router;
